49fce9c99816b4936ff34cb203c09553264c960a
Andrey Shingiriy <andrey.shingiriy@gmail.com>
2026-04-10 23:50:28 +0300

    fix(install.sh): Enhance installation script with improved status messages and silent operations for better user experience
    Signed-off-by: Andrey Shingiriy <andrey.shingiriy@gmail.com>

--- tools/install.sh
+++ tools/install.sh
@@ -18,45 +18,58 @@
 OS_TYPE=$(uname -s)
 
 if [ "$OS_TYPE" = "Linux" ]; then
-    echo "This is a Linux system."
+    echo " 🐧 This is a Linux system."
     if [ -f /etc/os-release ]; then
         # Load the distribution data
         . /etc/os-release
-        echo "OS Name: $NAME"
-        echo "ID: $ID" # e.g., ubuntu, fedora, arch
+        # echo "OS Name: $NAME"
+        echo " 🖥️ ID: $ID" # e.g., ubuntu, fedora, arch
         if [ "$ID" != "ubuntu" ] && [ "$ID" != "debian" ]; then
-            echo "This script is designed for Debian-based distributions. Exiting."
+            echo " 🚨 This script is designed for Debian-based distributions. Exiting."
             exit 1
         fi
-        sudo apt update -qq
-        sudo apt upgrade -y -qq
+        echo " 🏎️ Updating package lists and upgrading existing packages..."
+        sudo apt update -qq > /dev/null
+        sudo apt upgrade -y -qq > /dev/null
+        echo " ✅ done"
         if [ "$ID" = "ubuntu" ]; then
             version=$(lsb_release -rs)
             if [ "$version" = "25.10" ]; then
-                echo "🪳 Your Ubuntu version is: $version with bug: https://github.com/Aloxaf/fzf-tab/issues/549"
-                echo "💉 Installing workaround for fzf-tab plugin..."
-                sudo apt install coreutils-from-gnu coreutils-from-uutils- --allow-remove-essential -y
+                echo " 🪳 Your Ubuntu version is: $version with bug: https://github.com/Aloxaf/fzf-tab/issues/549"
+                echo " 💉 Installing workaround for fzf-tab plugin..."
+                sudo apt install coreutils-from-gnu coreutils-from-uutils- --allow-remove-essential -y > /dev/null
+                echo " ✅ done"
                 # -o APT::Get::Assume-Yes=true -o APT::Get::force-yes=true
             fi
         fi
+    echo " 🏎️ Installing Zsh, Fontconfig, Git, and FZF..."
     sudo apt install zsh fontconfig git fzf -y -qq
-    mkdir -p ~/.local/share/fonts
-    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Regular.ttf  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Bold_Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-    fc-cache -f -v
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    echo " ✅ done"
+    echo " 🏎️ Installing MesloLGS NF fonts..."
+    mkdir -p ~/.local/share/fonts > /dev/null
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Regular.ttf  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf > /dev/null
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf > /dev/null
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf > /dev/null
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Bold_Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf > /dev/null
+    fc-cache -f -v > /dev/null
+    echo " ✅ done"
+    echo " 🏎️ Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null
     # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    echo " ✅ done"
+    echo " 🏎️ Installing Oh My Zsh plugins and themes..."
+    git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab > /dev/null
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions > /dev/null
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting > /dev/null
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" > /dev/null
+    echo " ✅ done"
+    echo " 🏎️ Downloading configs for p10k and oh-my-zsh"
     curl -L -o ~/.p10k.zsh https://raw.githubusercontent.com/ashcycling/oh-my-zsh-on-debian/refs/heads/main/config/.p10k.zsh
     curl -L -o ~/.zshrc https://raw.githubusercontent.com/ashcycling/oh-my-zsh-on-debian/refs/heads/main/config/.zshrc
+    echo " ✅ done"
     else
-        echo "Could not find /etc/os-release"
+        echo " 🚨 Could not find /etc/os-release"
     fi
 elif [ "$OS_TYPE" = "Darwin" ]; then
-    echo "This is macOS."
+    echo " 💻 This is macOS."
 fi
