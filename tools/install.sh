3ac6910abea16469c4a98de45c94da3f0e80f1c7
Andrey Shingiriy <andrey.shingiriy@gmail.com>
2026-04-10 18:29:30 +0300

    fix(install.sh): Enhance installation script for Debian-based systems with OS checks and improved commands
    Signed-off-by: Andrey Shingiriy <andrey.shingiriy@gmail.com>

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,6 @@
-# sudo apt update
-# sudo apt upgrade -y
-# sudo apt install zsh fontconfig git -y
+# sudo apt update 
+# sudo apt upgrade -y -qq
+# sudo apt install zsh fontconfig git -y -qq
 # mkdir -p ~/.local/share/fonts
 # curl -L -o ~/.local/share/fonts/MesloLGS_NF_Regular.ttf  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 # curl -L -o ~/.local/share/fonts/MesloLGS_NF_Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
@@ -24,12 +24,35 @@ if [ "$OS_TYPE" = "Linux" ]; then
         . /etc/os-release
         echo "OS Name: $NAME"
         echo "ID: $ID" # e.g., ubuntu, fedora, arch
+        if [ "$ID" != "ubuntu" ] && [ "$ID" != "debian" ]; then
+            echo "This script is designed for Debian-based distributions. Exiting."
+            exit 1
+        fi
+        sudo apt update 
+        sudo apt upgrade -y -qq
         if [ "$ID" = "ubuntu" ]; then
             version=$(lsb_release -rs)
             if [ "$version" = "25.10" ]; then
-                echo "Your Ubuntu version is: $version"
+                echo "🪳 Your Ubuntu version is: $version with bug: https://github.com/Aloxaf/fzf-tab/issues/549"
+                echo "💉 Installing workaround for fzf-tab plugin..."
+                sudo apt install coreutils-from-gnu coreutils-from-uutils- --allow-remove-essential
             fi
         fi
+    sudo apt install zsh fontconfig git -y -qq
+    mkdir -p ~/.local/share/fonts
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Regular.ttf  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+    curl -L -o ~/.local/share/fonts/MesloLGS_NF_Bold_Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+    fc-cache -f -v
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    curl -L -o ~/.p10k.zsh https://raw.githubusercontent.com/ashcycling/oh-my-zsh-on-debian/refs/heads/main/config/.p10k.zsh
+    curl -L -o ~/.zshrc https://raw.githubusercontent.com/ashcycling/oh-my-zsh-on-debian/refs/heads/main/config/.zshrc
     else
         echo "Could not find /etc/os-release"
     fi
