0b6c24c45e7b8aa59bf852500a12e7e22be6d5d3
Andrey Shingiriy <andrey.shingiriy@gmail.com>
2026-04-14 15:46:25 +0300

    fix(fzf.sh): enable automatic download and installation of the latest fzf version
    Signed-off-by: Andrey Shingiriy <andrey.shingiriy@gmail.com>

--- tools/fzf.sh
+++ tools/fzf.sh
@@ -6,6 +6,10 @@
 # Установка/обновление последней версии fzf
 fzf_ver=$(curl  "https://api.github.com/repos/junegunn/fzf/tags" | jq -r '.[0].name')
 echo $fzf_ver
+curl -LO https://github.com/junegunn/fzf/releases/download/$fzf_ver/fzf-$fzf_ver-linux_amd64.tar.gz
+tar -xzf fzf-$fzf_ver-linux_amd64.tar.gz
+sudo mv fzf /usr/local/bin/fzf --version
+rm fzf-$fzf_ver-linux_amd64.tar.gz
 # curl -LO https://deckhouse.ru/downloads/deckhouse-cli/$d8_ver/d8-$d8_ver-darwin-arm64.tar.gz
 # tar -xvf "d8-$d8_ver-darwin-arm64.tar.gz" "darwin-arm64/bin/d8"
 # sudo mv "darwin-arm64/bin/d8" /usr/local/bin/
