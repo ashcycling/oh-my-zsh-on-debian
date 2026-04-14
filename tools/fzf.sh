3abd4a6c10c43fd61dc115187e766fef512e5601
Andrey Shingiriy <andrey.shingiriy@gmail.com>
2026-04-14 15:42:32 +0300

    fix(fzf.sh): update fzf installation to fetch the latest version dynamically
    Signed-off-by: Andrey Shingiriy <andrey.shingiriy@gmail.com>

--- tools/fzf.sh
+++ tools/fzf.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-curl -LO https://github.com/junegunn/fzf/releases/download/v0.71.0/fzf-0.71.0-linux_amd64.tar.gz
-tar -xzf fzf-0.71.0-linux_amd64.tar.gz
-sudo mv fzf /usr/local/bin/fzf --version
-rm fzf-0.71.0-linux_amd64.tar.gz
+# curl -LO https://github.com/junegunn/fzf/releases/download/v0.71.0/fzf-0.71.0-linux_amd64.tar.gz
+# tar -xzf fzf-0.71.0-linux_amd64.tar.gz
+# sudo mv fzf /usr/local/bin/fzf --version
+# rm fzf-0.71.0-linux_amd64.tar.gz
+# Установка/обновление последней версии fzf
+fzf_ver=$(curl  "https://api.github.com/repos/junegunn/fzf/tags" | jq -r '.[0].name')
+echo $fzf_ver
+# curl -LO https://deckhouse.ru/downloads/deckhouse-cli/$d8_ver/d8-$d8_ver-darwin-arm64.tar.gz
+# tar -xvf "d8-$d8_ver-darwin-arm64.tar.gz" "darwin-arm64/bin/d8"
+# sudo mv "darwin-arm64/bin/d8" /usr/local/bin/
