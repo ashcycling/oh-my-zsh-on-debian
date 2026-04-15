de13139af51faddc94ffdeefa60abfb7b24484ca
Andrey Shingiriy <andrey.shingiriy@gmail.com>
2026-04-15 13:14:37 +0300

    fix(fzf.sh): correct syntax for moving fzf binary and remove tar file
    Signed-off-by: Andrey Shingiriy <andrey.shingiriy@gmail.com>

--- tools/fzf.sh
+++ tools/fzf.sh
@@ -8,8 +8,8 @@ fzf_ver=$(curl -s  "https://api.github.com/repos/junegunn/fzf/tags" | jq -r '.[0
 # echo $fzf_ver
 curl -sfLO https://github.com/junegunn/fzf/releases/download/v$fzf_ver/fzf-$fzf_ver-linux_amd64.tar.gz
 tar -xzf fzf-$fzf_ver-linux_amd64.tar.gz > /dev/null 2>&1
-sudo mv fzf /usr/local/bin/fzf /dev/null 2>&1
-rm fzf-$fzf_ver-linux_amd64.tar.gz /dev/null 2>&1
+sudo mv fzf /usr/local/bin/fzf > /dev/null 2>&1
+rm fzf-$fzf_ver-linux_amd64.tar.gz > /dev/null 2>&1
 # curl -LO https://deckhouse.ru/downloads/deckhouse-cli/$d8_ver/d8-$d8_ver-darwin-arm64.tar.gz
 # tar -xvf "d8-$d8_ver-darwin-arm64.tar.gz" "darwin-arm64/bin/d8"
 # sudo mv "darwin-arm64/bin/d8" /usr/local/bin/
